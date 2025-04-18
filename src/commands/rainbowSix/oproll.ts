2b345a1a00f0b8581302dafa286b0f1ba584b8bb
Saiki <115924991+Quodlun@users.noreply.github.com>
2025-04-18 14:25:20 +0800

    重構 oproll.ts 檔案，將隨機操作員選擇邏輯移至單獨函數

--- src/commands/rainbowSix/oproll.ts
+++ src/commands/rainbowSix/oproll.ts
@@ -3,46 +3,54 @@ import { ChatInputCommandInteraction } from "discord.js"
 import { SlashCommandBuilder } from "@discordjs/builders"
 import { atk_ops, def_ops } from "@data/ops";
 
-export default
+function randomOperator ( side: any )
+{
+  var random_num;
+
+  switch ( side )
   {
-    data: new SlashCommandBuilder()
-      .setName('oproll')
-      .setDescription('Pick an operator to play.')
-      .addStringOption
-      (Option => Option.setName("side")
-        .setDescription("Select which side to roll.")
-        .setRequired(true)
-        .addChoices
-        (
-          {
-            name: "ATK",
-            value: "atk"
-          },
+    case "atk":
+      random_num = Math.floor ( ( Math.random () * 10000 ) % atk_ops.length);
+      return atk_ops [ random_num ];
+      break;
 
-          {
-            name: "DEF",
-            value: "def"
-          },
-        )
-      ),
+    case "def":
+      random_num = Math.floor((Math.random() * 10000) % def_ops.length);
+      return def_ops [ random_num ];
+      break;
 
-    async execute(_client: BotClient, interaction: ChatInputCommandInteraction) {
-      var random_num;
-      switch (interaction.options.getString("side")) {
-        case "atk":
-          random_num = Math.floor((Math.random() * 10000) % atk_ops.length);
-          await interaction.reply(`Operator: ${atk_ops[random_num]}`);
+    default:
+      break;
+  }
+}
 
-          break;
+export default
+{
+  data: new SlashCommandBuilder ()
+    .setName ( 'oproll' )
+    .setDescription ( 'Pick an operator to play.' )
 
-        case "def":
-          random_num = Math.floor((Math.random() * 10000) % def_ops.length);
-          await interaction.reply(`Operator: ${def_ops[random_num]}`);
+    .addStringOption
+    (Option => Option.setName("side")
+      .setDescription("Select which side to roll.")
+      .setRequired(true)
+      .addChoices
+      (
+        {
+          name: "ATK",
+          value: "atk"
+        },
 
-          break;
+        {
+          name: "DEF",
+          value: "def"
+        },
+      )
+    ),
 
-        default:
-          break;
-      }
-    },
-  };
+  async execute(_client: BotClient, interaction: ChatInputCommandInteraction) 
+  {
+    const operator = randomOperator ( interaction.options.getString ( "side" ) );
+    await interaction.reply ( `Operator: ${ operator }` );
+  }
+};
