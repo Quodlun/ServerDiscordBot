d523e912ae7dc8c382a3b9a5283f4f590772f9fe
Quodlun <115924991+Quodlun@users.noreply.github.com>
2025-04-21 12:55:46 +0800

    修正 fetchIp 函式以返回動態 IP 地址，取代固定 IP 地址

--- src/data/fetchIp.ts
+++ src/data/fetchIp.ts
@@ -2,7 +2,7 @@ export async function fetchIp (): Promise < string >
 {
   const respond = await fetch ( 'https://api.ipify.org?format=json' );
   const data = await respond.json ();
-  let ip: string = "1.163.68.121" //data.ip;
+  let ip: string = data.ip;
 
   return ip;
 }
