c55a091ad2098cef848f6b7a5b8e986d705fdd2d
Quodlun <115924991+Quodlun@users.noreply.github.com>
2025-04-23 01:16:16 +0800

    更新依賴項，將 r6s-stats-api 替換為 r6api.js

--- moduleInstall.bat
+++ moduleInstall.bat
@@ -1,3 +1,3 @@
 @echo off
 
-npm i typescript rimraf module-alias dotenv discord.js axios @napi-rs/canvas r6s-stats-api
+npm i typescript rimraf module-alias dotenv discord.js axios @napi-rs/canvas r6api.js
